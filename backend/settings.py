32dd5c47c17112262a8421f6a28eca1a54af58af
Brian Okello <okellobrian18@gmail.com>
2026-04-02 12:44:12 +0300

    Db connection and model registration

--- backend/settings.py
+++ backend/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'registration',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -74,11 +78,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'voter_db',
+        'USER': 'voter_user',
+        'PASSWORD': 'securepassword123',   # your password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -115,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOW_ALL_ORIGINS = True   # for development only
+# Or specify your React dev server:
+# CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Applicant
 
-# Register your models here.
+@admin.register(Applicant)
+class ApplicantAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'phone_number', 'id_number', 'county', 'voter_status', 'registration_channel', 'registered_at')
+    list_filter = ('registration_channel', 'county', 'voter_status')
+    search_fields = ('full_name', 'phone_number', 'id_number')

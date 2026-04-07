ff80e4675d5c1b5218b0c0d102d08331007fdd2a
Brian Okello <okellobrian18@gmail.com>
2026-04-07 11:52:06 +0300

    live linc

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_BASE = '/api';  // uses proxy (see package.json later)
+const API_BASE = 'https://voter-registration-gn1a.onrender.com/api';  // uses proxy (see package.json later)
 
 export const registerViaWeb = (data) => axios.post(`${API_BASE}/register/web/`, data);
 export const fetchApplicants = () => axios.get(`${API_BASE}/applicants/`);
